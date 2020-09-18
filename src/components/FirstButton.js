41c5e8057b3f47d1e51ea1d500464093c9555e87
adihd <47402232+adihd@users.noreply.github.com>
2020-09-18 10:38:41 +0300

    co

--- src/components/FirstButton.js
+++ src/components/FirstButton.js
@@ -3,14 +3,14 @@ import useInputState from "../hooks/useInputState";
 import TextField from "@material-ui/core/TextField";
 
 function FirstButton({ id, task, editTodo, toggleEditForm }) {
-  const [name, setName] = useState("Adi");
+  const [name, setName] = useState(true);
 
   function chooseCousin(params) {}
 
   return (
     <div>
-      <button onClick={() => setName("Nevo")}> Who is the best cousin? </button>
-      <h1>wait for it {name}</h1>
+      <button onClick={() => setName(!name)}> Who is the best cousin? </button>
+      <h1>wait for it {name ? "Adi" : "Nevo"}</h1>
     </div>
   );
 }
