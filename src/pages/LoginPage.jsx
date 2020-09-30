2b88cad51bd5fca5d4e095310bc8a4342b04baea
Adi <adi.straussa@gmail.co>
2020-09-30 23:53:17 +0300

    iii

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,17 +3,17 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Checkbox from "@material-ui/core/Checkbox";
+// import Link from "@material-ui/core/Link";
+// import Grid from "@material-ui/core/Grid";
+// import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import * as io from "socket.io-client";
-
+var socket;
 socket = io("http://localhost:5000");
 
 socket.on("login_response", (reply) => {
@@ -111,6 +111,7 @@ export default function LoginPage() {
             label="Remember me"
           /> */}
           <Button
+            onClick="clickLogin"
             type="submit"
             fullWidth
             variant="contained"
