401f03fcc71bfa315b0395717a266dd6152bf354
Adi <adi.straussa@gmail.co>
2020-10-05 19:17:00 +0300

    11

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -3,11 +3,11 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Checkbox from "@material-ui/core/Checkbox";
+// import Link from "@material-ui/core/Link";
+// import Grid from "@material-ui/core/Grid";
+// import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
@@ -49,17 +49,16 @@ export default function LoginPage() {
       // then:
       history.push("/");
     } else {
-      // todo show the client the msg
       alert(reply.message);
     }
   });
 
   const onSubmitFunc = (e) => {
     e.preventDefault();
-    const { username1, password1 } = state;
+    const { username, password } = state;
     socket.emit("login_attempt", {
-      username: username1,
-      password: password1,
+      username: username,
+      password: password,
     });
   };
 
@@ -117,7 +116,7 @@ export default function LoginPage() {
             Log In
           </Button>
         </form>
-        <h1>{state.username}</h1>
+        {/* <h1>{state.username}</h1> */}
       </div>
     </Container>
   );
